de6f002c97116afb47edb0a96da67a11ee902f7a
sasank-desaraju <sasank.desaraju@ufl.edu>
2023-03-13 19:47:54 -0400

    plotting val images serially like test images

--- scripts/lit_KPResNet.py
+++ scripts/lit_KPResNet.py
@@ -109,6 +109,7 @@ class KeypointNetModule(pl.LightningModule):
         # * Logging the predictions
         # Must remember that val_output is a tensor of shape (batch_size, 2 * num_keypoints)
         # And x is a tensor of shape (batch_size, 1, self.image_height, self.image_width)
+        """
         fig_output = plot_val_images(images=full_val_batch, preds=val_output, labels=val_batch_labels, img_names=img_names, num_keypoints=self.num_keypoints, title='Unsubsetted Image')
         self.wandb_run.log({f'validation/val_output_{batch_idx}': fig_output})
         # Plot the model output from what the model actually sees
@@ -117,7 +118,17 @@ class KeypointNetModule(pl.LightningModule):
         # Just plot the input images
         fig_input = plot_inputs(images=full_val_batch, img_names=img_names, title='Input Image')
         self.wandb_run.log({f'validation/epoch_{str(self.current_epoch)}_val_input_{batch_idx}': fig_input})
+        """
 
+        # Use plot_test_images to plot the images
+        fig_output_vector = plot_test_images(images=full_val_batch, preds=val_output, labels=val_batch_labels, img_names=img_names, num_keypoints=self.num_keypoints, title='Unsubsetted Image')
+        fig_subsetted_output_vector = plot_test_images(images=val_batch, preds=val_output, labels=val_batch_labels, img_names=img_names, num_keypoints=self.num_keypoints, title='Model View')
+        fig_intput_vector = plot_test_inputs(images=full_val_batch, img_names=img_names, title='Input Image')
+
+        for i in range(len(fig_output_vector)):
+            self.wandb_run.log({f'validation/{img_names[i]}/full_output': fig_output_vector[i]})
+            self.wandb_run.log({f'validation/{img_names[i]}/subsetted_output': fig_subsetted_output_vector[i]})
+            self.wandb_run.log({f'validation/{img_names[i]}/input': fig_intput_vector[i]})
 
         return loss
 
